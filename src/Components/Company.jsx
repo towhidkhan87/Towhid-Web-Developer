c1b25d6e0468f4459f4e03103f7644a2a2774193
towhidkhan87 <towhid01717307254@gmail.com>
2025-04-12 14:29:44 +0600

    first commit

---
+++ src/Components/Company.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Company = () => {
+    return (
+        <div>
+            <div className="flex flex-col lg:flex-row justify-between items-center w-11/12 mx-auto py-5 gap-10 lg:w-9/12 slide-in-right-lg">
+                <img src="/src/assets/image/aust_logo.svg" className="w-40 h-40 mx-auto py-5" />
+                <img src="/src/assets/image/BBF-LOGO.png" className="w-40 h-40 mx-auto py-5" />
+                <img src="/src/assets/image/Interspeed_logo_menu_5.png" className="w-40 h-40 mx-auto py-5" />
+                <img src="/src/assets/image/SCB_logo.png" className="w-40 h-40 mx-auto py-5" />
+                <img src="/src/assets/image/logo_uce.jpg" className="w-40 h-40 mx-auto py-5" />
+                <img src="/src/assets/image/logo_Nwave.jpg" className="w-40 h-40 mx-auto py-5" />
+            </div>
+        </div>
+    );
+};
+
+export default Company;
